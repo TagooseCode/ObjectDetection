713f1b689a0a3a6d32f73037b0d771b7c8cec11f
Ori Mizrahi <omizrahi@ucsb.edu>
2020-04-26 14:33:34 -0700

    Initial Commit

---
+++ ObjectDetection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectDetection
+//
+//  Created by Ori Mizrahi on 4/26/20.
+//  Copyright © 2020 bigo technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
